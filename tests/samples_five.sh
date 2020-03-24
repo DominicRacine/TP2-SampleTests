cd118a3417722010ff5cf992cea11e94d53b0503
Charlie Gauthier <31974070+Velythyl@users.noreply.github.com>
2020-03-24 12:10:05 -0400

    Update samples_five.sh

--- tests/samples_five.sh
+++ tests/samples_five.sh
@@ -1,4 +1,5 @@
 blocker,echo&2,20&2&1&1&20
 blocker || echo je_passe_quand_meme_avant_dawg &
 r2(blocker) || f1(blocker) && echo muahahha_je_passe_avant_toi && echo surprised_pikachu.jpg && sleep 5 &
+sleep 100
 exit
